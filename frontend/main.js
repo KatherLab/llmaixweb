9db510de480cb3e2700a37a7ed37fefe39c77729
Fabian Wolf <fabian@fawolf.de>
2025-07-22 14:34:53 +0200

    Fix toastification
    Signed-off-by: Fabian Wolf <fabian@fawolf.de>

--- frontend/main.js
+++ frontend/main.js
@@ -4,13 +4,15 @@ import App from './App.vue'
 import router from './router'
 import './assets/main.css'
 import { ModuleRegistry, AllCommunityModule } from 'ag-grid-community';
-import { themeBalham } from 'ag-grid-community';
+import Toast from 'vue-toastification';
+import 'vue-toastification/dist/index.css';
 
-const app = createApp(App)
-app.use(createPinia())
-app.use(router)
+const app = createApp(App);
+app.use(createPinia());
+app.use(router);
 ModuleRegistry.registerModules([AllCommunityModule]);
-app.mount('#app')
+app.mount('#app');
+app.use(Toast);
 
 window.addEventListener('unhandledrejection', (event) => {
   console.error('Unhandled Promise Rejection:', event.reason);
