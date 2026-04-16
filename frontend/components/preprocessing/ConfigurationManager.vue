dc90345cba851769ae421597fa2643d2d9684c3d
FWao <fabian@fawolf.de>
2026-04-16 13:33:13 +0200

    Fix css background-image url

--- frontend/components/preprocessing/ConfigurationManager.vue
+++ frontend/components/preprocessing/ConfigurationManager.vue
@@ -744,7 +744,7 @@ const deleteConfiguration = async (config) => {
   width: 0.75rem !important;
   height: 0.75rem !important;
   display: block !important;
-  background-image: url(\"data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='none'%3E%3Cpath stroke='%23ffffff' stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M6 18L18 6M6 6l12 12'/%3E%3C/svg%3E\") !important;
+  background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='none'%3E%3Cpath stroke='%23ffffff' stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M6 18L18 6M6 6l12 12'/%3E%3C/svg%3E") !important;
   background-size: contain !important;
   background-repeat: no-repeat !important;
   background-position: center !important;
